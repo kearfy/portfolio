b3d9a584f80e7527f6c6d27b4312d09cfafe879e
Kearfy <mt.dev@hotmail.com>
2019-09-15 18:11:34 +0200

    sync

---
+++ script.js
@@ -0,0 +1,15 @@
+function updatenav() {
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) === false) {
+        var nav = document.querySelector('div.navbar');
+        if (window.scrollY > document.querySelector('section').scrollHeight - nav.scrollHeight) {
+            nav.style.background = '#e5e5e5';
+            nav.style.transition = 'all 0.1s ease-in';
+        } else {
+            nav.style.background = 'unset';
+            nav.style.transition = 'unset';
+        }
+    }
+}
+
+window.onscroll = (e) => { updatenav(); };
+updatenav();
